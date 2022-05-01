3e4b28752ddc48322cb00f7c50677f936292ed49
abhishek <abc@gmail.com>
2022-05-01 12:54:12 +0530

    deleted redundant files

--- services/bakery/knowledge_assistant_permission.py
+++
@@ -1,8 +0,0 @@
-from pydantic.main import BaseModel
-from typing import List
-from shared.application.permissions.services import Services
-
-
-class KnowledgeAssistantPermission(BaseModel):
-    service: str = Services.KNOWLEDGE_ASSISTANT
-    permissions: List[str]
